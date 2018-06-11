4228e1992fe091e8eadeeab9dbf4bf77f3b9c9a0
Lukasz Pierzan <lukasz.pierzan@tomtom.com>
2018-06-11 11:50:40 +0200

    Move query to xml file (no problem with query format)

--- src/main/java/com/tomtom/athena/sample/Application.java
+++ src/main/java/com/tomtom/athena/sample/Application.java
@@ -2,8 +2,10 @@ package com.tomtom.athena.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:queries-beans.xml")
 public class Application {
 
     public static void main(
--- src/main/java/com/tomtom/athena/sample/db/AthenaRepository.java
+++ src/main/java/com/tomtom/athena/sample/db/AthenaRepository.java
@@ -2,18 +2,15 @@ package com.tomtom.athena.sample.db;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.commons.lang3.text.StrSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.google.common.collect.Maps;
-import com.tomtom.athena.sample.config.QueriesConfiguration;
 import com.tomtom.athena.sample.db.entities.BusinessesInStateWithReviewsOverview;
 
 @Component
@@ -23,25 +20,20 @@ public class AthenaRepository {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Qualifier("athenaQuery")
     @Autowired
-    private QueriesConfiguration queriesConfiguration;
+    private String topReviewedStatesQuery;
 
 
     public Collection<BusinessesInStateWithReviewsOverview> getTopReviewedBusinesses() {
         log.info("Querying for business with reviews overview records:");
-        String query = queriesConfiguration.getTopReviewedStatesQuery();
-        List<BusinessesInStateWithReviewsOverview> businesses = jdbcTemplate.query(adjustQuerySchema(query),
+        String query = topReviewedStatesQuery;
+        List<BusinessesInStateWithReviewsOverview> businesses =
+            jdbcTemplate.query(query,
             new BeanPropertyRowMapper<BusinessesInStateWithReviewsOverview>(BusinessesInStateWithReviewsOverview.class));
         businesses.forEach(business -> log.info(business.toString()));
         return businesses;
-    }
-
-    private String adjustQuerySchema(
-        final String query) {
-        Map<String, String> substitutionMap = Maps.newHashMap();
-        substitutionMap.put("schema", queriesConfiguration.getSchema());
-        StrSubstitutor substitutor = new StrSubstitutor(substitutionMap);
-        return substitutor.replace(query);
     }
 
 }
